1f345fb9343e55bbd50793b3966f0afd404d82fd
Jesse Friedland <jesse.friedland@disneystreaming.com>
2020-06-01 15:08:18 -0400

    Output script to a log file

--- start-service.sh
+++ start-service.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -xe
+
+exec > /home/ec2-user/app/logs/startup.log 2>&1
+
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
-java -jar target/aws-boostrap-spring-boot-docker-0.0.1-SNAPSHOT.jar &
+java -jar target/aws-boostrap-spring-boot-docker-*.jar &
 echo $! > ./pid.file
 #docker run --publish=8080:8080 aws-bootstrap-spring-boot-docker &
--- stop-service.sh
+++ stop-service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
+pid_file_name=pid.file
 [ -d "/home/ec2-user/app/release" ] && \
 cd /home/ec2-user/app/release && \
-kill $(cat ./pid.file)
+[ -f "$pid_file_name" ] && kill $(cat ./$pid_file_name)
 #docker stop aws-bootstrap-spring-boot-docker
